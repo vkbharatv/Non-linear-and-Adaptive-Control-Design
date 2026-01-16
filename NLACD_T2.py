12315a9bdcee197307a5391f831cd74132259a6c
bharat.verma <bharat.verma>
2026-01-16 20:20:37 +0530

    update

--- NLACD_T2.py
+++ NLACD_T2.py
@@ -4,6 +4,10 @@ import control as ctrl
 import numpy as np
 import matplotlib.pyplot as plt
 from DelayControlSystem.delay_control import *
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.family"] = "Times New Roman"
+plt.style.use("seaborn-v0_8")
 # %%
 """
 1. Delay simulation and effect of approximation
@@ -24,15 +28,13 @@ t = np.linspace(0,T,int(T/dt))
 t1, y1 = ctrl.step_response(G1, t)
 t2, y2 = ctrl.step_response(G2, t)
 
-
-print(t)
 # %%
 G3 = DelayControlSystem(G, delay_time=theta, dt=dt)
 G3.reset()
 y_t = []
 t_n = []
 for t_i in t:
-    x,y,t = G3.step(u=1.0)
+    x, y, _ = G3.step(u=1.0)
     y_t.append(y[0])
     t_n.append(t_i)
 
@@ -66,19 +68,30 @@ plt.show()
 # %%
 
 G_delay = DelayControlSystem(G, delay_time=theta, dt=dt)
-C = 1 + 0.5 / s
-r = np.ones_like(t)
+s = ctrl.TransferFunction.s  # Ensure s is defined as a transfer function variable
+C = 0.2 + 0.2 / s
 C = DelayControlSystem(C, delay_time=0, dt=dt)
 G_delay.reset()
 C.reset()
-y_T=[]
-t=np.linspace(0,T,int(T/dt))
-for i,t_i in enumerate(t):
-    e = r[i] - G_delay.y[-1]
-    x_c, u_c, t_c = C.step(e)
-    x, y, t_s = G_delay.step(u_c)
+y_T = [0]
+T = 100
+t_sim = np.linspace(0, T, int(T / dt))
+r = np.ones_like(t_sim)
+
+for i, t_i in enumerate(t_sim):
+    e = r[i] - y_T[-1]
+    _, u_c, _ = C.step(e)
+    _, y, _ = G_delay.step(u_c)
     y_T.append(y[0])
-plt.plot(t, r, 'g--', label='Reference r(t)')
-plt.plot(t, y_T, 'b-', label='Output y(t)')
-plt.legend()
+
+plt.plot(t_sim, y_T[1:], label="Output y(t) with Delay")
+plt.plot(t_sim, r, "r--", label="Reference r(t)")
+plt.grid(True, which="minor", alpha=0.3)
+plt.grid(True, which="major", alpha=0.7)
+plt.xlabel("Time (s)")
+plt.minorticks_on()
+plt.ylabel("Output")
+plt.title("System Response with Delay and PID Control")
 plt.show()
+
+# %%
